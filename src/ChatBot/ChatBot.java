53a4e606adf5e7d5164f5d95e61c9aa9e9cd555e
Denis <d.y.hrek@student.khai.edu>
2021-12-20 16:23:57 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+    }
+}
+
