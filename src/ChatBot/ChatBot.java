b753ea239e76377e738e07c4efa0bf9c6f730c80
Denis <d.y.hrek@student.khai.edu>
2021-12-20 16:26:11 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -7,6 +7,9 @@ public class ChatBot {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
 
